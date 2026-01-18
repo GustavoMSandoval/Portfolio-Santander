d804f4ea1ca2601d10ad4378eee73fd65ad52e4a
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-18 18:44:13 -0300

    feat: Adicionando menu responsivo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
+import { FiAlignJustify, FiX } from "react-icons/fi";
+
 export default function Navbar() {
+
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!isOpen);
+  }
+
   return (
-    <header>
-        <nav className="flex">
-            <a href=""></a>
-            <ul className="nav-link-list">
-                <li><a href="start" className="nav-link">Início</a></li>
-                <li><a href="about" className="nav-link">Sobre</a></li>
-                <li><a href="experience" className="nav-link">Experiência</a></li>
-                <li><a href="contact" className="nav-link">Contato</a></li>
+    <header className="fixed w-full top-1 z-50">
+      <nav className="navbar md:hidden">
+        <div className="flex items-center gap-30 sm:gap-64">
+          <a href="#">
+            <img className="w-12" src="/banco-santander-logo.png" alt="Logo branca Santander" />
+          </a>
+          <div>
+            <button className="text-white text-4xl" onClick={toggleMenu}>{isOpen ? <FiX/> : <FiAlignJustify/>}</button>
+          </div>
+        </div>
+        <div className={`overflow-hidden transition-all duration-700 ease-in-out ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
+          <ul className="nav-link-list flex flex-col items-end gap-4">
+            <li className="nav-link-item"><a href="inicio" className="nav-link">Início</a></li>
+            <li className="nav-link-item"><a href="sobre" className="nav-link">Sobre</a></li>
+            <li className="nav-link-item"><a href="experiencia" className="nav-link">Experiência</a></li>
+            <li className="nav-link-item"><a href="contato" className="nav-link">Contato</a></li>
+          </ul>
+        </div>
+      </nav>
+      <nav className="navbar hidden md:flex items-center">
+            <a href="#">
+              <img className="w-12" src="/banco-santander-logo.png" alt=""/>
+            </a>
+            <ul className="nav-link-list flex gap-4">
+                <li className="nav-link-item"><a href="inicio" className="nav-link">Início</a></li>
+                <li className="nav-link-item"><a href="sobre" className="nav-link">Sobre</a></li>
+                <li className="nav-link-item"><a href="experiencia" className="nav-link">Experiência</a></li>
+                <li className="nav-link-item"><a href="contato" className="nav-link">Contato</a></li>
             </ul>
-            <img src="" alt=""/>
+            <div>
+              <img className="rounded-full w-12" src="/foto_bonita.jpg" alt=""/>
+            </div>
         </nav>
     </header>
   )
