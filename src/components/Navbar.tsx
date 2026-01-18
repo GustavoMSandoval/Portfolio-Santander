9a6c7204cf5f2d69d6f7426c6aa6b1d961f147b2
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-18 15:44:57 -0300

    feat: Adicionando navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <header>
+        <nav className="flex">
+            <a href=""></a>
+            <ul className="nav-link-list">
+                <li><a href="start" className="nav-link">Início</a></li>
+                <li><a href="about" className="nav-link">Sobre</a></li>
+                <li><a href="experience" className="nav-link">Experiência</a></li>
+                <li><a href="contact" className="nav-link">Contato</a></li>
+            </ul>
+            <img src="" alt=""/>
+        </nav>
+    </header>
+  )
+}
