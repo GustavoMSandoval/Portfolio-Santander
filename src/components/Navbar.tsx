008004bfa4f4b499df5c76ee6f4135571429e429
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-18 20:54:40 -0300

    fix: Consertando nav-links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,10 +22,10 @@ export default function Navbar() {
         </div>
         <div className={`overflow-hidden transition-all duration-700 ease-in-out ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}>
           <ul className="nav-link-list flex flex-col items-end gap-4">
-            <li className="nav-link-item"><a href="inicio" className="nav-link">Início</a></li>
-            <li className="nav-link-item"><a href="sobre" className="nav-link">Sobre</a></li>
-            <li className="nav-link-item"><a href="experiencia" className="nav-link">Experiência</a></li>
-            <li className="nav-link-item"><a href="contato" className="nav-link">Contato</a></li>
+            <li className="nav-link-item"><a href="#inicio" className="nav-link">Início</a></li>
+            <li className="nav-link-item"><a href="#sobre" className="nav-link">Sobre</a></li>
+            <li className="nav-link-item"><a href="#experiencia" className="nav-link">Experiência</a></li>
+            <li className="nav-link-item"><a href="#contato" className="nav-link">Contato</a></li>
           </ul>
         </div>
       </nav>
@@ -34,10 +34,10 @@ export default function Navbar() {
               <img className="w-12" src="/banco-santander-logo.png" alt=""/>
             </a>
             <ul className="nav-link-list flex gap-4">
-                <li className="nav-link-item"><a href="inicio" className="nav-link">Início</a></li>
-                <li className="nav-link-item"><a href="sobre" className="nav-link">Sobre</a></li>
-                <li className="nav-link-item"><a href="experiencia" className="nav-link">Experiência</a></li>
-                <li className="nav-link-item"><a href="contato" className="nav-link">Contato</a></li>
+                <li className="nav-link-item"><a href="#inicio" className="nav-link">Início</a></li>
+                <li className="nav-link-item"><a href="#sobre" className="nav-link">Sobre</a></li>
+                <li className="nav-link-item"><a href="#experiencia" className="nav-link">Experiência</a></li>
+                <li className="nav-link-item"><a href="#contato" className="nav-link">Contato</a></li>
             </ul>
             <div>
               <img className="rounded-full w-12" src="/foto_bonita.jpg" alt=""/>
