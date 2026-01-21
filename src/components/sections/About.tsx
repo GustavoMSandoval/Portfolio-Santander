66c7d8eb2cade3370ee0fab6971f1b04c58cd347
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-21 09:49:12 -0300

    fix: Arrumando responsividade

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,19 +1,19 @@
 export default function AboutSection() {
   return (
     <section id="sobre">
-        <div className="section-container flex flex-col items-center gap-12">
+        <div className="section-container flex flex-col lg:items-center gap-12">
             <h2 className="section-title">
                 sobre mim
             </h2>
-            <div className="flex flex-col gap-5">
-                  <p className="text-center font-bold">
+            <div className="flex flex-wrap flex-col gap-5 w-2xl  md:w-full about-text">
+                  <p className="lg:text-center font-bold">
                     Olá! Me chamo Gustavo, e sou um desenvolvedor movido pelo desafio de transformar linhas de código em soluções de alto impacto.
                   </p>
                   <div className="flex flex-col gap-5">
-                    <p className="text-center">
-                        Acredito que a tecnologia só cumpre seu papel quando gera valor real. Por isso, minha atuação é guiada por três pilares fundamentais <br/> que venho aplicando em meus projetos recentes:
+                    <p className="lg:text-center">
+                        Acredito que a tecnologia só cumpre seu papel quando gera valor real. Por isso, minha atuação é guiada por três pilares fundamentais <br className="hidden lg:block"/> que venho aplicando em meus projetos recentes:
                     </p>
-                      <div className="flex flex-col items-center w-3xl mx-auto  md:my-6">
+                      <div className="flex flex-col lg:items-center lg:w-3xl lg:mx-auto  md:my-6">
                         <ul className="about-list ">
                                 <li>
                                     <p><span className="font-bold">Foco em Resultados:</span> Entrega de soluções que impactam diretamente o bottom line, resultando em redução de custos operacionais e otimização de recursos.</p>
@@ -27,15 +27,15 @@ export default function AboutSection() {
                         </ul>
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 mx-auto w-4xl">
+                <div className="flex flex-col flex-wrap gap-4 mx-auto w-4xl">
                     <h3 className="section-subtitle">Por que o Santander?</h3>
-                    <div className="text-center flex flex-col gap-4">
+                    <div className="lg:text-center flex flex-wrap flex-col gap-4">
                         <p>Minha sede de conhecimento e minha atenção rigorosa aos detalhes encontram eco na cultura de excelência do Santander. Vejo no banco o ambiente ideal para quem deseja inovar com responsabilidade, buscando sempre a melhoria contínua.</p>
                         <p>Meu objetivo é alinhar minha dedicação técnica aos objetivos estratégicos da instituição, ajudando a construir o futuro dos serviços financeiros.</p>
                     </div>
                 </div>
-                <div className="mx-auto">
-                    <ul className="technology-list flex flex-wrap gap-2">
+                <div className="lg:mx-auto">
+                    <ul className="technology-list flex flex-wrap gap-0.5 lg:gap-2">
                         <li className="technology-list-item">UI/UX</li>
                         <li className="technology-list-item">HTML5</li>
                         <li className="technology-list-item">CSS3</li>
